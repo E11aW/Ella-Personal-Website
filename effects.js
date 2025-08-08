bd2436e2f96d13e47499fc3449b038f10d9b14e8
E11aW <Ellaraindeer2022@gmail.com>
2025-08-08 15:10:51 -0700

    updated assets

--- effects.js
+++ effects.js
@@ -39,6 +39,51 @@ function animateRaindrops() {
     requestAnimationFrame(animateRaindrops);
 }
 
+const floatingClouds = document.querySelectorAll('.floating-cloud');
+let cloudPositions = [];
+
+function animateClouds() {
+    const speed = 1;
+
+    floatingClouds.forEach((cloud, i) => {
+        cloudPositions[i] -= speed;
+        if (cloudPositions[i] <= -cloud.offsetWidth) {
+            // loop the cloud back to the others side of the screen
+            const maxX = Math.max(...cloudPositions);
+            cloudPositions[i] = maxX + cloud.offsetWidth;
+        }
+        cloud.style.left = cloudPositions[i] + 'px';
+    });
+    requestAnimationFrame(animateClouds);
+}
+
+
+function fadeIn(elementID) {
+    const element = document.getElementById(elementID);
+    let opacity = 0;
+    element.style.opacity = 0;
+    element.style.transition = 'opacity 2s ease-in';
+    requestAnimationFrame(() => {
+        element.style.opacity = 0.9;
+    });
+}
+
+// call functions
+const backgroundImage = new Image();
+backgroundImage.src = "Assets/background.png";
+backgroundImage.onload = function () {
+    fadeIn('top-cloud');
+}
+
+window.addEventListener('load', () => {
+    floatingClouds.forEach((cloud, i) => {
+        const startX = i * cloud.offsetWidth;
+        cloud.style.left = startX + 'px';
+        cloudPositions[i] = startX;
+    });
+    animateClouds();
+});
+
 setInterval(spawnRaindrop, 25);
 
 requestAnimationFrame(animateRaindrops);
